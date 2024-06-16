534990323f8d0270ceb467ff1e9cbd9161016b7c
reynaldohalim <reynaldohalim.rh@gmail.com>
2024-06-16 19:01:46 +0700

    Initiating FCM (for sending notifications)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBMzXCBjNCbmzsLWCS36W8cBzwZVDNyj7o",
+    authDomain: "account-center-app.firebaseapp.com",
+    projectId: "account-center-app",
+    storageBucket: "account-center-app.appspot.com",
+    messagingSenderId: "566841473898",
+    appId: "1:566841473898:android:6dfd93d919a765d7a08313"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBMzXCBjNCbmzsLWCS36W8cBzwZVDNyj7o",
+    authDomain: "account-center-app.firebaseapp.com",
+    projectId: "account-center-app",
+    storageBucket: "account-center-app.appspot.com",
+    messagingSenderId: "566841473898",
+    appId: "1:566841473898:android:6dfd93d919a765d7a08313"
+  }
 };
 
 /*
